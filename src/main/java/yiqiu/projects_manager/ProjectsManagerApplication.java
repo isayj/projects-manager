767d018a4e99782943f0fb4d606cb6ae8d0ede7c
Isa yj <isayjnai@outlook.com>
2024-07-24 06:56:37 +0200

    first commit

---
+++ src/main/java/yiqiu/projects_manager/ProjectsManagerApplication.java
@@ -0,0 +1,13 @@
+package yiqiu.projects_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yiqiu/projects_manager/ProjectsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package yiqiu.projects_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
